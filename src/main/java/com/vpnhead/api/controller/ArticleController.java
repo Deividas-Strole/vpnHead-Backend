359368b23fa8c1f392b641dc8d5b28b0826f94a3
deividas strole <deiwis@yahoo.com>
2025-06-06 10:01:14 -0700

    wip

--- src/main/java/com/vpnhead/api/controller/ArticleController.java
+++ src/main/java/com/vpnhead/api/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.vpnhead.api.controller;
 import com.vpnhead.api.dto.ArticleTitleDTO;
 import com.vpnhead.api.model.Article;
 import com.vpnhead.api.service.ArticleService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:5173")
